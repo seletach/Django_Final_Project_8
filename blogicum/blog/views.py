5b8c49da61b6f18c4c8719c1e0b127b13bd8ba3f
Ilya Lykianov <ilkanov106@gmail.com>
2024-04-04 21:35:59 +0800

    rewiew

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,20 +1,37 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post, Category
 
 
 def index(request):
     template_name = 'blog/index.html'
-    return render(request, template_name)
+    post_list = Post.objects.filter(
+        is_published=True,
+    )
+    context = {
+        'post_list': post_list,
+    }
+    return render(request, template_name, context)
 
 
 def post_detail(request, id):
     template_name = 'blog/detail.html'
-    try:
-        return render(request, template_name)
-    except IndexError:
-        raise Http404
+    post = get_object_or_404(
+        Post,
+        id=id)
+    context = {
+        'post': post,
+    }
+    return render(request, template_name, context)
 
 
 def category_posts(request, category_slug):
     template_name = 'blog/category.html'
-    return render(request, template_name, {'category_slug': category_slug})
+    category = get_object_or_404(Category,
+                                 slug=category_slug,
+                                 is_published=True)
+    posts = Post.objects.filter(category=category, is_published=True)
+    context = {
+        'category': category,
+        'posts': posts
+    }
+    return render(request, template_name, context)
